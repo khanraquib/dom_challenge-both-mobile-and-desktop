091beb212ab52c637e07a3d02ea664b3f75a49ad
khanraquib <57582855+khanraquib@users.noreply.github.com>
2020-03-07 21:24:41 +0530

    else block added

--- script.js
+++ script.js
@@ -3,24 +3,47 @@ function myFun() {
     var last = document.getElementById("last_name").value;
     var mail = document.getElementById("email").value;
     var pass = document.getElementById("passwor").value;
-    if (first == "") {
+   
+   
+        if (first == "") {
         document.getElementById("message").innerHTML = "First name cannot be empty ";
+        document.getElementById("first_name").style.border = "1px  solid red";
         return false;
+    }
+    else  {
+            document.getElementById("message").innerHTML = " ";
+            document.getElementById("first_name").style.border = "1px  solid green";
+           
     }
    
     if (last == "") {
         document.getElementById("message1").innerHTML = "Last name cannot be empty ";
+        document.getElementById("last_name").style.border = "1px solid red";
         return false;
+    }
+    else  {
+        document.getElementById("message1").innerHTML = " ";
+        document.getElementById("last_name").style.border = "1px  solid green";
     }
 
     if (mail == "") {
         document.getElementById("message4").innerHTML = "Looks like this is not an email ";
+        document.getElementById("email").style.border = "1px solid red";
         return false;
     }
- 
+    else  {
+        document.getElementById("message1").innerHTML = " ";
+        document.getElementById("email").style.border = "1px  solid green";
+    }
     if (pass == "") {
         document.getElementById("message3").innerHTML = "Password cannot be empty ";
+        document.getElementById("passwor").style.border = "1px solid red";
         return false;
+    }
+
+    else  {
+        document.getElementById("message1").innerHTML = " ";
+        document.getElementById("email").style.border = "1px  solid green";
     }
 }
 
